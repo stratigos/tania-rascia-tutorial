a90cd1410435797b6ef4343f6922c57525e80690
Todd <stratigos@users.noreply.github.com>
2019-10-29 10:07:38 -0700

    Add Form Component 📄

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react'
+import Form from './Form'
 import Table from './Table'
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee Snyder',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ],
+    characters: [],
   }
 
   removeCharacter = index => {
@@ -33,12 +17,21 @@ class App extends Component {
     })
   }
 
+  handleSubmit = character => {
+    this.setState(
+      {
+        characters: [...this.state.characters, character]
+      }
+    )
+  }
+
   render() {
     const { characters } = this.state
 
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     )
   }
