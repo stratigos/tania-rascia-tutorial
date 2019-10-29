f9ee95be4d6503a9e6d6324bc5081e97a2fb2a3e
Todd <stratigos@users.noreply.github.com>
2019-10-29 09:46:22 -0700

    Use Props and State 📊
    * Pass `characterData` Array from `App` (as though it were fetched
      and parsed JSON), down into `Table`, and from there to `TableBody`
    * Pass the `characters` as state to dependent Components
    * Pass a delete function down from `App` to `TableBody` so it can
      set its `onClick` property (attr?) (also as state)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react'
 import Table from './Table'
 
 class App extends Component {
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee Snyder',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ],
+  }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
   render() {
+    const { characters } = this.state
+
     return (
       <div className="container">
-        <Table />
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
       </div>
     )
   }
